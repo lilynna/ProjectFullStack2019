21807b6aee9704c5483ff113e9aecf3d2dea2624
DarkUna <gungunploy05@gmail.com>
2019-06-06 15:15:31 +0700

    create class

--- smalllibrary/tubook/models.py
+++ smalllibrary/tubook/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Binding(models.Model):
+    name = name = models.CharField(max_length=255)
+
+class Publisher(models.Model):
+    name = name = models.CharField(max_length=255)
+
+class book(models.Model):
+    title = models.CharField(max_length=255)
+    ISBN_10 = models.CharField(max_length=255)
+    Author = models.CharField(max_length=255)
+    Binding = models.ForeignKey(Binding, 
+                                on_delete=models.SET_NULL, 
+                                null=True)
+    Year = models.PositiveSmallInteger()
+    Publisher = models.ForeignKey(Publisher, 
+                                on_delete=models.SET_NULL, 
+                                null=True)
