7f8ee27c74615d8ac43e02eaf35d18c77449cd16
DarkUna <gungunploy05@gmail.com>
2019-06-06 16:30:35 +0700

    Add book Binding Publisher

--- smalllibrary/tubook/admin.py
+++ smalllibrary/tubook/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Book
+from .models import Publisher
+from .models import Binding
 
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-     list_display =['id','Title','ISBN_10','Author','Binding','Year','Publisher']
+     lists_display =['id','Title','ISBN_10','Author','Binding','Year','Publisher']
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+     lists_display =['id','name']
+
+@admin.register(Binding)
+class BindingAdmin(admin.ModelAdmin):
+     lists_display =['id','name']
+
+
+
--- smalllibrary/tubook/models.py
+++ smalllibrary/tubook/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.                                 
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Binding(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     ISBN_10 = models.CharField(max_length=255)
@@ -9,27 +20,17 @@ class Book(models.Model):
     Binding = models.ForeignKey(Binding, 
                                 on_delete=models.SET_NULL, 
                                 null=True)
-    Year = models.PositiveSmallInteger()
+    Year = models.PositiveSmallIntegerField()
     Publisher = models.ForeignKey(Publisher, 
                                 on_delete=models.SET_NULL, 
                                 null=True)
     def __str__(self):
-        return self.name
-                                    
+        return self.title
+
 class Borrow(models.Model):
     Borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     Book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     def __str__(self):
-        return self.name
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=255)
-     def __str__(self):
-        return self.name
-
-class Binding(models.Model):
-    name = models.CharField(max_length=255)
-     def __str__(self):
         return self.name
 
 class Transaction(models.Model):
